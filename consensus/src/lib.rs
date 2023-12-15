43e9f80f2dcb98a462de179a13737433d13e3df7
nkramer44 <noah.ph.kramer@gmail.com>
2023-12-15 11:54:28 -0500

    don't log if no ancestors

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -189,7 +189,7 @@ impl Consensus {
             info!("on_timeout, preferred ({:?} {}), latest ledger ({:?} {}) ", preferred_id, preferred_seq, self.latest_ledger.id, self.latest_ledger.seq);
 
             if preferred_id != self.latest_ledger.id() {
-                if *self.latest_ledger.ancestors.last().unwrap() == preferred_id {
+                if !self.latest_ledger.ancestors.is_empty() && *self.latest_ledger.ancestors.last().unwrap() == preferred_id {
                     error!("We just switched to {:?}'s parent {:?}", self.latest_ledger.id, preferred_id);
                 }
                 warn!(
